4ef56a4779124c196ea80e0b9b798c93f2c32a9b
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-19 19:35:43 -0500

    Landing Page added

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Heroes = () => {
+  return <div>Heroes</div>;
+};
+
+export default Heroes;
--- app/test/page.tsx
+++
@@ -1,5 +0,0 @@
-const TestPage = () => {
-  return <div>Hello Test Page</div>;
-};
-
-export default TestPage;
