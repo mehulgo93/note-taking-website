71fada59f22c5552aa2defc3cbb7485fe2afa8a1
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-22 13:57:36 -0500

    adding more functionality

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useQuery, useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { useState } from "react";
-import { Id } from "@/convex/_generated/dataModel";
-import { toast } from "sonner";
-import { Spinner } from "@/components/spinners";
 import { Search, Trash, Undo } from "lucide-react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Spinner } from "@/components/spinners";
 import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 export const TrashBox = () => {
   const router = useRouter();
@@ -26,25 +28,28 @@ export const TrashBox = () => {
   const onClick = (documentId: string) => {
     router.push(`/documents/${documentId}`);
   };
+
   const onRestore = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
     documentId: Id<"documents">
   ) => {
     event.stopPropagation();
     const promise = restore({ id: documentId });
+
     toast.promise(promise, {
       loading: "Restoring note...",
       success: "Note restored!",
-      error: "Failed to restore note.",
+      error: " Failed to restore note.",
     });
   };
 
   const onRemove = (documentId: Id<"documents">) => {
-    const promise = restore({ id: documentId });
+    const promise = remove({ id: documentId });
+
     toast.promise(promise, {
       loading: "Deleting note...",
       success: "Note deleted!",
-      error: "Failed to delete note.",
+      error: " Failed to delete note.",
     });
 
     if (params.documentId === documentId) {
@@ -68,11 +73,11 @@ export const TrashBox = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
-          placeholder="Filter by page title...."
+          placeholder="Filter by page title..."
         />
       </div>
       <div className="mt-2 px-1 pb-1">
-        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2 ">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
           No documents found.
         </p>
         {filteredDocuments?.map((document) => (
@@ -87,16 +92,18 @@ export const TrashBox = () => {
               <div
                 onClick={(e) => onRestore(e, document._id)}
                 role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>
-              <div
-                role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
-              >
-                <Trash className="h-4 w-4 text-muted-foreground" />
-              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+                >
+                  <Trash className="h-4 w-4 text-muted-foreground" />
+                </div>
+              </ConfirmModal>
             </div>
           </div>
         ))}
