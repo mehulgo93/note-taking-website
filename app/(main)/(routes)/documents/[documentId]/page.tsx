91ed8c2ee2412f66bef57205939b1a6dde4625f8
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-22 22:24:46 -0500

    adding document edit functionality

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,37 @@
-const DocumentPage = () => {
-  return <div>DocumentId</div>;
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentPage = ({ params }: DocumentIdProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentPage;
